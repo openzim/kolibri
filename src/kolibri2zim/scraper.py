61cb1549a8237284b847094f7e9dc0c16f54ef9a
benoit74 <benoit74@users.noreply.github.com>
2024-02-27 14:54:29 +0100

    Fix handling of failures to stop the scraper and display all issues encountered

--- src/kolibri2zim/scraper.py
+++ src/kolibri2zim/scraper.py
@@ -179,13 +179,9 @@ class Kolibri2Zim:
     def populate_nodes_executor(self):
         """Loop on content nodes to create zim entries from kolibri DB"""
 
-        def remove_future(future):
-            self.nodes_futures.remove(future)
-
         def schedule_node(item):
             future = self.nodes_executor.submit(self.add_node, item=item)
             self.nodes_futures.add(future)
-            future.add_done_callback(remove_future)
 
         # schedule root-id
         schedule_node((self.db.root["id"], self.db.root["kind"]))
@@ -876,7 +872,7 @@ class Kolibri2Zim:
             self.populate_nodes_executor()
 
             # await completion of all futures (nodes and videos)
-            result = cf.wait(
+            futures = cf.wait(
                 self.videos_futures | self.nodes_futures,
                 return_when=cf.FIRST_EXCEPTION,
             )
@@ -886,19 +882,20 @@ class Kolibri2Zim:
             # only awaits future completion and doesn't include callbacks
             self.videos_executor.shutdown()
 
-            succeeded = (
-                not result.not_done
-                and sum([1 if fs.exception() else 0 for fs in result.done]) == 0
+            nb_done_with_failure = sum(
+                [1 if future.exception() else 0 for future in futures.done]
             )
+            succeeded = not futures.not_done and nb_done_with_failure == 0
 
-            # DEBUG: raise first exception
-            if not succeeded and result.done:
-                logger.info(
-                    f"FAILURE not_done={len(result.not_done)} done={len(result.done)}"
+            if not succeeded:
+                logger.warning(
+                    f"FAILURE: not_done={len(futures.not_done)}, "
+                    f"done successfully={len(futures.done) - nb_done_with_failure}, "
+                    f"done with failure={nb_done_with_failure}"
                 )
-                for future in result.done:
-                    if future.exception():
-                        raise future.exception()  # pyright:ignore
+                for future in [fut for fut in futures.done if fut.exception()]:
+                    logger.warning("", exc_info=future.exception())
+                raise Exception("Some nodes have not been processed successfully")
         except KeyboardInterrupt:
             self.creator.can_finish = False
             logger.error("KeyboardInterrupt, exiting.")
